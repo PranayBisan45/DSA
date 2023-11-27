7215c64fc7ac4b08ddaaefcdf4dd2909a07ec886
PranayBisan45 <pranaybisan45@gmail.com>
2023-11-27 23:30:30 +0530

    Day 7 notes added

--- data structure class notes/Day 2/SortingTechniques/src/TestSelectionSort.java
+++ data structure class notes/Day 2/SortingTechniques/src/TestSelectionSort.java
@@ -20,7 +20,7 @@ public class TestSelectionSort
 			//assum the i th number is smallest
 			int min_idx=i;
 			//find the position of minimum number in the array
-			for(int j=i+1;j<n;j++)
+			for(int j=i+1;j<n-1;j++)
 			{
 				if(arr[j]<arr[min_idx])
 				{
